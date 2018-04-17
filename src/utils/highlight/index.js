37a372c4aff8cd6996a85a18ac60f48b246dc93a
BroccoliSpring <Lensgcx@163.com>
2018-04-17 14:48:02 +0800

    utils
    utils

---
+++ src/utils/highlight/index.js
@@ -0,0 +1,12 @@
+// highlight.js
+import Hljs from 'highlight.js'
+let Highlight = {}
+Highlight.install = function (Vue, options) {
+  Vue.directive('highlight', function (el) {
+    let blocks = el.querySelectorAll('pre code');
+    blocks.forEach((block) => {
+      Hljs.highlightBlock(block);
+    })
+  })
+}
+export default Highlight
