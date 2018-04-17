e5d96a6a99e4d68513fbf6911fd1bd03e0fa65bd
BroccoliSpring <Lensgcx@163.com>
2018-04-17 14:49:02 +0800

    App
    App

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,36 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <router-view></router-view>
   </div>
 </template>
-
 <script>
-export default {
-  name: 'App'
-}
+  export default {
+    name: 'app',
+    created(){
+
+    },
+    methods: {
+      //编码
+      html_encode(str) {
+        var s = "";
+        if (str.length == 0) return "";
+        s = str.replace(/&/g, ">");
+        s = s.replace(/</g, "<");
+        s = s.replace(/>/g, ">");
+        s = s.replace(/ /g, " ");
+        s = s.replace(/\'/g, "'");
+        s = s.replace(/\"/g, '"');
+        s = s.replace(/\n/g, "<br>");
+        return s;
+      },
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
 </style>
