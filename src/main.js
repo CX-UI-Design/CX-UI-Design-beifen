1ef932f3ad780fc893cc873d4b63f3b31e105c21
BroccoliSpring <Lensgcx@163.com>
2018-04-17 14:49:10 +0800

    main
    main

--- src/main.js
+++ src/main.js
@@ -2,14 +2,23 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import router from './router/index'
+import store from './store/index'
 
+import components from './components'
 Vue.config.productionTip = false
 
+// Import view related components (UI, animation, attachments, scripts...)
+import viewSpt from './views'
+import './mock'
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  store,
+  viewSpt,
+  components,
+  // axios,
+  components: {App},
   template: '<App/>'
 })
