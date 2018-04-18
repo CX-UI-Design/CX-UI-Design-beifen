1c79a5855d4843c1b8799578979825a4942fcbbd
BroccoliSpring <Lensgcx@163.com>
2018-04-18 23:14:14 +0800

    back-to-top component
    back-to-top component

--- src/components/BackToTop/index.vue
+++ src/components/BackToTop/index.vue
@@ -1,112 +1,130 @@
+<!--back-to-top-->
 <template>
-    <transition :name="transitionName">
-        <div class="back-to-top" @click="backToTop" v-show="visible" :style="customStyle">
-            <svg width="16" height="16" viewBox="0 0 17 17" xmlns="http://www.w3.org/2000/svg" class="Icon Icon--backToTopArrow" aria-hidden="true"
-                 style="height: 16px; width: 16px;">
-                <title>回到顶部</title>
-                <g>
-                    <path d="M12.036 15.59c0 .55-.453.995-.997.995H5.032c-.55 0-.997-.445-.997-.996V8.584H1.03c-1.1 0-1.36-.633-.578-1.416L7.33.29c.39-.39 1.026-.385 1.412 0l6.878 6.88c.782.78.523 1.415-.58 1.415h-3.004v7.004z"
-                          fill-rule="evenodd"></path>
-                </g>
-            </svg>
-        </div>
-    </transition>
+  <transition :name="transitionName">
+    <el-tooltip :placement="placement" :content="tooltipTit" :effect="tooltipTheme" :disabled="tooltipTit?false:true">
+      <div class="cx-back-to-top" @click="backToTop" v-show="visible" :style="customStyle">
+        <svg width="16" height="16" viewBox="0 0 17 17" xmlns="http://www.w3.org/2000/svg" class="cx-backToTop-svg Icon--backToTopArrow" aria-hidden="true"
+             style="height: 16px; width: 16px;">
+          <title>回到顶部</title>
+          <g>
+            <path
+              d="M12.036 15.59c0 .55-.453.995-.997.995H5.032c-.55 0-.997-.445-.997-.996V8.584H1.03c-1.1 0-1.36-.633-.578-1.416L7.33.29c.39-.39 1.026-.385 1.412 0l6.878 6.88c.782.78.523 1.415-.58 1.415h-3.004v7.004z"
+              fill-rule="evenodd"></path>
+          </g>
+        </svg>
+      </div>
+    </el-tooltip>
+  </transition>
 </template>
 
 <script>
-    export default {
-        name: 'BackToTop',
-        props: {
-            visibilityHeight: {
-                type: Number,
-                default: 400
-            },
-            backPosition: {
-                type: Number,
-                default: 0
-            },
-            customStyle: {
-                type: Object,
-                default: {
-                    right: '50px',
-                    bottom: '50px',
-                    width: '40px',
-                    height: '40px',
-                    'border-radius': '4px',
-                    'line-height': '45px',
-                    background: '#e7eaf1'
-                }
-            },
-            transitionName: {
-                type: String,
-                default: 'fade'
-            }
-        },
-        data() {
-            return {
-                visible: false,
-                interval: null
-            }
-        },
-        mounted() {
-            window.addEventListener('scroll', this.handleScroll);
-        },
-        beforeDestroy() {
-            window.removeEventListener('scroll', this.handleScroll);
-            if (this.interval) {
-                clearInterval(this.interval);
-            }
-        },
-        methods: {
-            handleScroll() {
-                this.visible = window.pageYOffset > this.visibilityHeight;
-            },
-            backToTop() {
-                const start = window.pageYOffset;
-                let i = 0;
-                this.interval = setInterval(() => {
-                    const next = Math.floor(this.easeInOutQuad(10 * i, start, -start, 300));
-                    if (next <= this.backPosition) {
-                        window.scrollTo(0, this.backPosition);
-                        clearInterval(this.interval)
-                    } else {
-                        window.scrollTo(0, next);
-                    }
-                    i++;
-                }, 16.7)
-            },
-            easeInOutQuad(t, b, c, d) {
-                if ((t /= d / 2) < 1) return c / 2 * t * t + b;
-                return -c / 2 * (--t * (t - 2) - 1) + b;
-            }
+  export default {
+    name: 'cx-back-to-top',
+    props: {
+      actionNode: {type: String, default: 'window'},
+      visibilityHeight: {type: Number, default: 600},
+      backPosition: {type: Number, default: 0},
+      transitionName: {type: String, default: 'fade'},
+      tooltipTit: {type: String},
+      tooltipTheme: {type: String, default: 'light'},
+      placement: {type: String, default: 'top'},
+      customStyle: {
+        type: Object, default: function () {
+          return {
+            right: '20px',
+            bottom: '50px',
+            width: '40px',
+            height: '40px',
+            'border-radius': '4px',
+            'line-height': '45px',
+            background: '#e7eaf1'
+          }
         }
+      },
+    },
+    data() {
+      return {
+        node: null,//active node
+        visible: false,
+        interval: null
+      }
+    },
+    mounted() {
+      this.node = this.getNode('main');//get node
+      this.node.addEventListener('scroll', this.handleScroll);
+    },
+    beforeDestroy() {
+      this.node.removeEventListener('scroll', this.handleScroll);
+      if (this.interval) {
+        clearInterval(this.interval);
+      }
+    },
+    methods: {
+      /**
+       * get node
+       * @param node
+       * @returns {*}
+       */
+      getNode(node) {
+        if (typeof node !== 'string' || !node) return window;
+        if (node === 'body') {
+          return document.body;
+        }
+        else if (node === 'window') {
+          return window;
+        }
+        else {
+          const nid = document.getElementById(node);//node by ID
+          const nclass = document.getElementsByClassName(node)[0];//node by className
+          return nid ? nid : nclass ? nclass : window;
+        }
+      },
+      handleScroll() {
+        this.visible = this.node.scrollTop > this.visibilityHeight;
+      },
+      backToTop() {
+        const start = this.node.scrollTop;
+        let i = 0;
+        this.interval = setInterval(() => {
+          const next = Math.floor(this.easeInOutQuad(10 * i, start, -start, 300));
+          if (next <= this.backPosition) {
+            this.node.scrollTo(0, this.backPosition);
+            clearInterval(this.interval)
+          } else {
+            this.node.scrollTo(0, next);
+          }
+          i++;
+        }, 16.7)
+      },
+      easeInOutQuad(t, b, c, d) {
+        if ((t /= d / 2) < 1) return c / 2 * t * t + b;
+        return -c / 2 * (--t * (t - 2) - 1) + b;
+      }
     }
+  }
 </script>
 
-<style scoped>
-    .back-to-top {
-        position: fixed;
-        display: inline-block;
-        text-align: center;
-        cursor: pointer;
+<style rel="stylesheet/scss" lang="scss" scoped>
+  .cx-back-to-top {
+    position: fixed;
+    display: inline-block;
+    text-align: center;
+    cursor: pointer;
+    &:hover {
+      background: #d5dbe7;
     }
+    .cx-backToTop-svg {
+      fill: #9aaabf;
+      background: none;
+    }
+  }
 
-    .back-to-top:hover {
-        background: #d5dbe7;
-    }
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s;
+  }
 
-    .fade-enter-active,
-    .fade-leave-active {
-        transition: opacity .5s;
-    }
+  .fade-enter, .fade-leave-to {
+    opacity: 0
+  }
 
-    .fade-enter,
-    .fade-leave-to {
-        opacity: 0
-    }
-
-    .back-to-top .Icon {
-        fill: #9aaabf;
-        background: none;
-    }
 </style>
