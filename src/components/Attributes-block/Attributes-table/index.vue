5b09f59707deeebeb6c2d866f47cdca01fe8eabe
BroccoliSpring <Lensgcx@163.com>
2018-04-18 23:13:31 +0800

    components config
    components config

---
+++ src/components/Attributes-block/Attributes-table/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-table :data="tableData" class="cx-attributes-table">
+    <el-table-column v-for="(item,index) in tableHead" :key="index" :index="index" :label="item.label" :min-width="item.width">
+      <template slot-scope="scope">
+        <span v-html="scope.row[item.key]"></span>
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+<script>
+  export default {
+    name: 'cx-attributes-table',
+    props: {
+      tableHead: {type: Array},
+      tableData: {type: Array},
+    },
+    data() {
+      return {}
+    },
+    created() {
+
+    },
+  }
+</script>
+<style rel="stylesheet/scss" lang="scss">
+  .cx-attributes-table.el-table {
+    color: #333;
+    thead {
+      color: #666;
+      .cell {
+        text-align: left;
+        white-space: nowrap;
+        font-weight: 400;
+      }
+    }
+  }
+</style>
