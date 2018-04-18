2153134fd8a6b104fc11bbe54b68a29b485a5e48
BroccoliSpring <Lensgcx@163.com>
2018-04-18 10:20:13 +0800

    view config
    view config

--- src/views/index.js
+++ src/views/index.js
@@ -6,29 +6,17 @@ import Cookies from 'js-cookie';
 import Highlight from '../utils/highlight/index'
 /* ---------------- css / less /sass ----------------*/
 import 'cx-theme/dist/cx-theme.min.css'
+import '../assets/css/theme-cover.scss'//ele cover css
 import '../assets/css/public.scss'//public css
+import '../assets/css/modules/layout/layout.scss'//bench index style
+import '../assets/css/modules/front/front.scss'//front
 import '../utils/svgConfig'//iconfont - config
-//ele cover css
-import '../assets/css/element-ui/theme-cover/base.scss'
-import '../assets/css/element-ui/theme-cover/data.scss'
-import '../assets/css/element-ui/theme-cover/navigation.scss'
-import '../assets/css/element-ui/theme-cover/notice.scss'
-import '../assets/css/element-ui/theme-cover/other.scss'
-import '../assets/css/element-ui/theme-cover/form.scss'
-
-//bench index style
-import '../assets/css/modules/layout/layout.scss'
-//front
-import '../assets/css/modules/front/front.scss'
-
 
 /* ---------------- js ( Framework or auxiliary function ) ---------------- */
 import Vue from 'vue'
 import ElementUI from 'element-ui'
 import vueWaves from '../directive/waves';// waves command
 import formEvent from '../utils/formEvent'
-
-
 
 import CxEasyForm from 'cx-easy-form';
 import 'cx-easy-form/dist/cx-easy-form.min.css';
