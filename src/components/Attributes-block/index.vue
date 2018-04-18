d839ca95ee376c4a4dc1683c225e0c545179189f
BroccoliSpring <Lensgcx@163.com>
2018-04-18 23:13:44 +0800

    attributes-block
    attributes-block

---
+++ src/components/Attributes-block/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="cx-attributes-block">
+    <cx-sub-title>{{title}}</cx-sub-title>
+    <cx-attributes-table :tableData="tableData" :tableHead="tableHead"></cx-attributes-table>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'cx-attributes-block',
+    props: {
+      title: {type: String},
+      tableHead: {type: Array},
+      tableData: {type: Array},
+    },
+    data() {
+      return {}
+    },
+    created() {
+
+    },
+  }
+</script>
+<style rel="stylesheet/scss" lang="scss">
+  .cx-attributes-block {
+
+  }
+
+</style>
