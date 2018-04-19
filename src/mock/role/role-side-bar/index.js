7166ce66e2d88a89f6a0419475a7d427053778a2
BroccoliSpring <Lensgcx@163.com>
2018-04-19 22:20:33 +0800

    mock sidebar component
    mock sidebar component

--- src/mock/role/role-side-bar/index.js
+++ src/mock/role/role-side-bar/index.js
@@ -1,170 +1,133 @@
 const sideBarData = {
   main: [
-    //主页
+    //更新日志
     {
-      "groupName": "主页",
+      "groupName": "更新日志",
       "icon": "shezhi-",
       "firstIndex": "1",
+      "path": "/home/dashboard",
+      "level": 1,
+    },
+    //开发指南
+    {
+      "groupName": "开发指南",
+      "icon": "shezhi-",
+      "firstIndex": "2",
+      "level": 2,
       "children": [
         {
-          "id": "1-1",
-          "groupName": "主页",
+          "groupName": "安装",
+          "icon": "",
           "secondIndex": "1",
+          "path": "/home/dashboard"
+        },
+        {
+          "groupName": "快速上手",
+          "icon": "",
+          "secondIndex": "2",
+          "path": "/home/dashboard"
+        },
+        {
+          "groupName": "国际化",
+          "icon": "",
+          "secondIndex": "3",
           "path": "/home/dashboard"
         },
       ]
     },
+    //总览
     {
-      "groupName": "Basic",
-      "icon": "shezhi-",
-      "firstIndex": "2",
-      "children": [
-        {
-          "id": "2-1",
-          "groupName": "button",
-          "secondIndex": "1",
-          "path": "/Basic/buttonTest"
-        },
-        {
-          "id": "2-2",
-          "groupName": "Icon",
-          "secondIndex": "2",
-          "path": "/Basic/iconTest"
-        }
-      ]
-    },
-    {
-      "groupName": "Form",
+      "groupName": "总览",
       "icon": "shezhi-",
       "firstIndex": "3",
+      "level": 2,
       "children": [
         {
-          "id": "3-1",
-          "groupName": "radio",
+          "groupName": "演示",
+          "icon": "",
           "secondIndex": "1",
-          "path": "/Form/radioTest"
+          "path": "/home/dashboard"
         },
         {
-          "id": "3-2",
-          "groupName": "checkbox",
+          "groupName": "快速构",
+          "icon": "",
           "secondIndex": "2",
-          "path": "/Form/checkboxTest"
+          "path": "/home/dashboard"
         },
-        {
-          "id": "3-3",
-          "groupName": "select",
-          "secondIndex": "3",
-          "path": "/Form/selectTest"
-        },
-        {
-          "id": "3-4",
-          "groupName": "input",
-          "secondIndex": "4",
-          "path": "/Form/inputTest"
-        },
-        {
-          "id": "3-5",
-          "groupName": "cascader",
-          "secondIndex": "5",
-          "path": "/Form/cascaderTest"
-        },
-        {
-          "id": "3-6",
-          "groupName": "switch",
-          "secondIndex": "6",
-          "path": "/Form/switchTest"
-        },
-        {
-          "id": "3-7",
-          "groupName": "slider",
-          "secondIndex": "7",
-          "path": "/Form/sliderTest"
-        },
-        {
-          "id": "3-8",
-          "groupName": "form",
-          "secondIndex": "8",
-          "path": "/Form/formTest"
-        }
       ]
     },
+    //组件
     {
-      "groupName": "Data",
+      "groupName": "组件",
       "icon": "shezhi-",
       "firstIndex": "4",
+      "level": 3,
       "children": [
+        //Basic
         {
-          "id": "4-1",
-          "groupName": "progress",
+          "groupName": "Basic",
+          "icon": "",
           "secondIndex": "1",
-          "path": "/Data/progressTest"
+          "path": "/home/dashboard",
+          "children": [
+            {
+              "groupName": "图标",
+              "icon": "",
+              "thirdIndex": "1",
+              "path": "/home/dashboard",
+            },
+            {
+              "groupName": "按钮",
+              "icon": "",
+              "thirdIndex": "2",
+              "path": "/home/dashboard"
+            },
+          ]
         },
+        //normal
         {
-          "id": "4-2",
-          "groupName": "badge",
+          "groupName": "normal",
           "secondIndex": "2",
-          "path": "/Data/badgeTest"
+          "path": "/home/dashboard",
+          "children": [
+            {
+              "groupName": "图标",
+              "icon": "",
+              "thirdIndex": "1",
+              "path": "/home/dashboard",
+            },
+            {
+              "groupName": "按钮",
+              "icon": "",
+              "thirdIndex": "2",
+              "path": "/home/dashboard"
+            },
+          ]
         }
       ]
     },
-    {
-      "groupName": "Notice",
-      "icon": "shezhi-",
-      "firstIndex": "5",
-      "children": [
-        {
-          "id": "5-1",
-          "groupName": "alert",
-          "secondIndex": "1",
-          "path": "/Notice/alertTest"
-        }
-      ]
-    },
-    {
-      "groupName": "Navigation",
-      "icon": "shezhi-",
-      "firstIndex": "6",
-      "children": [
-        {
-          "id": "6-1",
-          "groupName": "dropDown",
-          "secondIndex": "1",
-          "path": "/Navigation/dropDownTest"
-        },
-        {
-          "id": "6-2",
-          "groupName": "tabs",
-          "secondIndex": "2",
-          "path": "/Navigation/tabsTest"
-        },
-        {
-          "id": "6-3",
-          "groupName": "steps",
-          "secondIndex": "3",
-          "path": "/Navigation/stepsTest"
-        }
-      ]
-    },
+
     {
       "groupName": "自动表单",
       "icon": "shezhi-",
-      "firstIndex": "7",
+      "firstIndex": "5",
+      "level": 2,
       "children": [
         {
-          "id": "7-1",
           "groupName": "说明",
+          "icon": "",
           "secondIndex": "1",
           "path": "/autoForm/index"
         },
         {
-          "id": "7-2",
           "groupName": "概览",
+          "icon": "",
           "secondIndex": "2",
           "path": "/autoForm/overview"
         },
       ]
     }
-  ],
+  ]
 }
 
 
@@ -177,3 +140,149 @@ export default {
     }
   },
 }
+
+// //主页
+// {
+//   "groupName": "主页",
+//   "icon": "shezhi-",
+//   "firstIndex": "1",
+//   "children": [
+//   {
+//     "id": "1-1",
+//     "groupName": "主页",
+//     "secondIndex": "1",
+//     "path": "/home/dashboard"
+//   },
+// ]
+// },
+// {
+//   "groupName": "Basic",
+//   "icon": "shezhi-",
+//   "firstIndex": "2",
+//   "children": [
+//   {
+//     "id": "2-1",
+//     "groupName": "button",
+//     "secondIndex": "1",
+//     "path": "/Basic/buttonTest"
+//   },
+//   {
+//     "id": "2-2",
+//     "groupName": "Icon",
+//     "secondIndex": "2",
+//     "path": "/Basic/iconTest"
+//   }
+// ]
+// },
+// {
+//   "groupName": "Form",
+//   "icon": "shezhi-",
+//   "firstIndex": "3",
+//   "children": [
+//   {
+//     "id": "3-1",
+//     "groupName": "radio",
+//     "secondIndex": "1",
+//     "path": "/Form/radioTest"
+//   },
+//   {
+//     "id": "3-2",
+//     "groupName": "checkbox",
+//     "secondIndex": "2",
+//     "path": "/Form/checkboxTest"
+//   },
+//   {
+//     "id": "3-3",
+//     "groupName": "select",
+//     "secondIndex": "3",
+//     "path": "/Form/selectTest"
+//   },
+//   {
+//     "id": "3-4",
+//     "groupName": "input",
+//     "secondIndex": "4",
+//     "path": "/Form/inputTest"
+//   },
+//   {
+//     "id": "3-5",
+//     "groupName": "cascader",
+//     "secondIndex": "5",
+//     "path": "/Form/cascaderTest"
+//   },
+//   {
+//     "id": "3-6",
+//     "groupName": "switch",
+//     "secondIndex": "6",
+//     "path": "/Form/switchTest"
+//   },
+//   {
+//     "id": "3-7",
+//     "groupName": "slider",
+//     "secondIndex": "7",
+//     "path": "/Form/sliderTest"
+//   },
+//   {
+//     "id": "3-8",
+//     "groupName": "form",
+//     "secondIndex": "8",
+//     "path": "/Form/formTest"
+//   }
+// ]
+// },
+// {
+//   "groupName": "Data",
+//   "icon": "shezhi-",
+//   "firstIndex": "4",
+//   "children": [
+//   {
+//     "id": "4-1",
+//     "groupName": "progress",
+//     "secondIndex": "1",
+//     "path": "/Data/progressTest"
+//   },
+//   {
+//     "id": "4-2",
+//     "groupName": "badge",
+//     "secondIndex": "2",
+//     "path": "/Data/badgeTest"
+//   }
+// ]
+// },
+// {
+//   "groupName": "Notice",
+//   "icon": "shezhi-",
+//   "firstIndex": "5",
+//   "children": [
+//   {
+//     "id": "5-1",
+//     "groupName": "alert",
+//     "secondIndex": "1",
+//     "path": "/Notice/alertTest"
+//   }
+// ]
+// },
+// {
+//   "groupName": "Navigation",
+//   "icon": "shezhi-",
+//   "firstIndex": "6",
+//   "children": [
+//   {
+//     "id": "6-1",
+//     "groupName": "dropDown",
+//     "secondIndex": "1",
+//     "path": "/Navigation/dropDownTest"
+//   },
+//   {
+//     "id": "6-2",
+//     "groupName": "tabs",
+//     "secondIndex": "2",
+//     "path": "/Navigation/tabsTest"
+//   },
+//   {
+//     "id": "6-3",
+//     "groupName": "steps",
+//     "secondIndex": "3",
+//     "path": "/Navigation/stepsTest"
+//   }
+// ]
+// },
