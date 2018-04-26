5b92d7460c6f1497d446f295eb8f71046d4004b4
BroccoliSpring <Lensgcx@163.com>
2018-04-26 23:32:59 +0800

    front
    front

--- src/views/front/index.vue
+++ src/views/front/index.vue
@@ -1,15 +1,23 @@
 <!--进入入口界面（切换登录，注册，忘记密码），初始：登录-->
 <template>
   <div class="front" id="front">
-    <!--changing-over  reg/reg/forget module-->
-    <transition enter-active-class="animated zoomInLeft" leave-active-class="animated zoomOutRight">
-      <router-view></router-view>
-    </transition>
+    <spot-wave-back :width="innerWidth" :height="innerHeight" :particleCount="10000">
+      <!--changing-over  reg/reg/forget module-->
+      <transition enter-active-class="animated zoomInLeft" leave-active-class="animated zoomOutRight">
+        <router-view></router-view>
+      </transition>
+    </spot-wave-back>
   </div>
 </template>
 <script>
   export default {
     name: 'front',
+    data() {
+      return {
+        innerWidth: window.innerWidth,
+        innerHeight: window.innerHeight,
+      }
+    }
   }
 </script>
 
