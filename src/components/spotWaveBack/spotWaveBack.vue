44ed6a6d874a1c3b7608db4abc4abc11898f2885
BroccoliSpring <Lensgcx@163.com>
2018-04-26 23:32:37 +0800

    spotWaveBack
    spotWaveBack

---
+++ src/components/spotWaveBack/spotWaveBack.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="spotWaveBack">
+    <canvas id="spotWaveBack_canvas"></canvas>
+    <slot></slot>
+  </div>
+</template>
+<script>
+  import ParticleWave from './spotWaveBack'
+
+  export default {
+    name: 'spotWaveBack',
+    props: {
+      width: {type: Number, default: 300},
+      height: {type: Number, default: 300},
+      particleCount: {type: Number, default: 30000},
+      background: {type: Number, default: 0x000000},
+      particle: {type: Number, default: 0x477cc2},
+    },
+    data() {
+      return {}
+    },
+    created() {
+
+    },
+    mounted() {
+      const config = {
+        colors: {
+          background: this.background,
+          particle: this.particle,
+        },
+        alpha: {
+          particle: 1,
+        },
+        size: {
+          width: this.width,
+          height: this.height,
+        },
+        particleCount: this.particleCount
+      };
+      let pw = new ParticleWave(config, 'spotWaveBack_canvas');
+      pw.run();
+    }
+  }
+</script>
+<style rel="stylesheet/scss" lang="scss" scoped>
+  .spotWaveBack {
+    #spotWaveBack_canvas {
+      display: block;
+      position: absolute;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+    }
+  }
+</style>
