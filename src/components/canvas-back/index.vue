6b7dd6284cf8e97568473e1cb719493be4204fb8
xilanhuadechuntian <Lensgcx@163.com>
2018-08-13 11:21:27 +0800

    canvas-back
    canvas-back

--- src/components/canvas-back/index.vue
+++ src/components/canvas-back/index.vue
@@ -4,6 +4,9 @@
     <glittering-stars :width="width" :height="height" v-if="type === 'stars'"></glittering-stars>
     <spot-wave :width="width" :height="height" :particleCount="particleCount" :background="background"
                :particle="particle" v-if="type === 'wave'"></spot-wave>
+    <matrix-raindrop :canvasWidth="width" :canvasHeight="height" :fontSize="fontSize" :speed="speed"
+                     :fontFamily="fontFamily" :textContent="textContent" :textColor="textColor"
+                     :backgroundColor="backgroundColor" v-if="type === 'raindrop'"></matrix-raindrop>
     <slot></slot>
   </div>
 </template>
@@ -17,11 +20,17 @@
       particleCount: {type: Number, default: 30000},
       background: {type: Number, default: 0x000000},
       particle: {type: Number, default: 0x477cc2},
+      //========== matrix-raindrop ==========
+      fontSize: {type: Number},  //下落字体大小
+      fontFamily: {type: String},  //字体类型
+      textContent: {type: String}, //字体文本内容
+      textColor: {type: String},//字体颜色
+      backgroundColor: {type: String}, //canvas背景颜色，可自定义
+      speed: {type: Number}   //下落速度
     },
     data() {
       return {}
     },
-
   }
 </script>
 <style rel="stylesheet/scss" lang="scss" scoped>
